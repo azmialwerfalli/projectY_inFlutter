90aa108a3935cece109d44c97070680882500a0a
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-10-03 18:06:28 +0200

    *google fonts Error Fixed (0.0.1/23.10.03V)

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromARGB(255, 250, 252, 255),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
@@ -15,8 +16,8 @@ class IntroPage extends StatelessWidget {
             // Name of project or Company Name
             Text(
               "Project Y",
-              style: GoogleFonts.dmMONO,
-            )
+              style: GoogleFonts.lato(fontSize: 28, color: Colors.black),
+            ),
 
             // icon
 
