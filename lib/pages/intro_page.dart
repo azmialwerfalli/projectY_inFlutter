6ce8e6aaa059004f32e6d8f2a5f116845fa8a607
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-10-03 13:03:02 +0200

    new file:   lib/Icons/box.png 	new file:   lib/Icons/canned-food.png 	new file:   lib/Icons/coffee-cup.png 	new file:   lib/Icons/delivery-truck.png 	new file:   lib/Icons/milk-box.png 	new file:   lib/Icons/milk.png 	modified:   lib/pages/intro_page.dart 	modified:   pubspec.yaml

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,11 +5,27 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Azmi Is Testing ",
-          style: TextStyle(fontSize: 30),
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 250, 252, 255),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            // Name of project or Company Name
+            Text(
+              "Project Y",
+              style: GoogleFonts.dmMONO,
+            )
+
+            // icon
+
+            // title
+
+            //subtitle
+
+            // get started button
+          ],
         ),
       ),
     );
